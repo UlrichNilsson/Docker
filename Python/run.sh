194d3889aa620eb037b2e83d60cd96384dee47f3
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2019-03-20 21:30:54 +0100

    Python example

---
+++ Python/run.sh
@@ -0,0 +1,12 @@
+#Build
+docker build --tag=friendlyhello .
+#Run
+docker run -d -p 4000:80 friendlyhello
+#Navigate to 
+#http://localhost:4000
+#Or
+#curl http://localhost:4000
+#TAG
+#docker tag friendlyhello ulrichnilsson/python:first_example
+#Push
+#docker push ulrichnilsson/python:first_example 
--- RabbiMQ/run.sh
+++ RabbiMQ/run.sh
@@ -1,2 +1,2 @@
 #docker run -d --hostname my-rabbit --name some-rabbit rabbitmq:3-management
-docker run -d --hostname rabbitmq_man --name rabbitmq_man2 -p 8080:15672 rabbitmq:3-management
+docker run -d --hostname rabbitmq_man --name rabbitmq_man -p 15672:15672 rabbitmq:3-management
