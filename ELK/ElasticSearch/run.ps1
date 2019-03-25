1bb1a64d7c71e9291fe0a302d6451a7a5536c977
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2019-03-25 22:24:34 +0100

    ELK

---
+++ ELK/ElasticSearch/run.ps1
@@ -0,0 +1,4 @@
+docker network create somenetwork
+
+
+docker run -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:6.6.2
---
+++ ELK/Kibana/run.ps1
@@ -0,0 +1,1 @@
+docker pull docker.elastic.co/kibana/kibana:6.6.2
