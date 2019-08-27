58d9756dcb1e1ba7ec1eff42400333e90c7441cb
Ulrich Nilsson <ulrichnilsson@Ulrichs-MBP.lan>
2019-08-27 22:19:17 +0200

    RabbitMQ docker-compose

--- RabbiMQ/run.sh
+++ RabbiMQ/run.sh
@@ -1,2 +1,6 @@
 #docker run -d --hostname my-rabbit --name some-rabbit rabbitmq:3-management
 docker run -d --hostname rabbitmq_man --name rabbitmq_man -p 15672:15672 rabbitmq:3-management
+
+
+
+docker-compose up -d
