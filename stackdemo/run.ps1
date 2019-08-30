dac5edad7b4f309682546854fc249c119eafa58d
Ulrich Nilsson <ulrichnilsson@Ulrichs-MBP.lan>
2019-08-30 16:32:56 +0200

    Portainer stack deploy, and verify stackdemo

--- stackdemo/run.ps1
+++ stackdemo/run.ps1
@@ -7,3 +7,8 @@ docker-compose ps
 docker stack deploy -c docker-compose.yml stackdemo
 #Stack remove
 docker stack rm stackdemo
+
+#Verify
+curl http://RP3S01:8001
+curl http://RP3S01:8001
+curl http://RP4S01:8001
