d1c92dbebc07e4ed483946a881d6c169fa6e8ab1
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2019-09-26 20:09:29 +0200

    .

---
+++ CoreApi/CoreApi/CoreApi.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CoreApi.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ CoreConsole/CoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
